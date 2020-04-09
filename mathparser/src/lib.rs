89f144c9f1b70381efbc70312b268648f0b0ea8e
Jakub Kądziołka <kuba@kadziolka.net>
2020-04-09 23:04:30 +0200

    Predicate grammar

--- mathparser/src/lib.rs
+++ mathparser/src/lib.rs
@@ -4,6 +4,17 @@ use num_bigint::BigUint;
 lalrpop_mod!(grammar);
 pub use grammar::ExprParser;
 
+/// Represents a primary command the bot might have as input, for example:
+///  - `round1(1, 2)`
+///  - `round1(1, 2) = 3`
+///  - `round1(x, y) = x + y`
+/// A query without a guess is just an expression, while the rest is a predicate.
+#[derive(Clone, Debug)]
+pub enum Command<'a> {
+    Pred(Pred<'a>),
+    Expr(Expr<'a>),
+}
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum BinOp {
     Add,
@@ -13,13 +24,28 @@ pub enum BinOp {
     Mod,
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cmp {
+    Eq,
+    Lt,
+    Le,
+    Gt,
+    Ge,
+}
+
 #[derive(Clone, Debug)]
 pub enum Expr<'a> {
     Func(&'a str, Vec<Expr<'a>>),
     Ident(&'a str),
+    If(Box<Pred<'a>>, Box<Expr<'a>>, Box<Expr<'a>>),
     BinOp(Box<Expr<'a>>, BinOp, Box<Expr<'a>>),
     Neg(Box<Expr<'a>>),
     Num(BigUint),
+}
+
+#[derive(Clone, Debug)]
+pub enum Pred<'a> {
+    Cmp(Expr<'a>, Cmp, Expr<'a>),
 }
 
 #[cfg(test)]
