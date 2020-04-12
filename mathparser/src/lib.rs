bbb4e2186be62067118ce14c4ffdd3dd9155573a
Jakub Kądziołka <kuba@kadziolka.net>
2020-04-12 18:14:43 +0200

    Make function evaluation work
    Evaluation errors now own their strings. Otherwise, the return value
    of `evaluate` would've had to borrow from both the expression and the
    context, as that's where function's expressions come from. However,
    contexts are generated on the fly, which wouldn't fly with borrowing
    errors.

--- mathparser/src/lib.rs
+++ mathparser/src/lib.rs
@@ -21,15 +21,27 @@ pub struct Context<'a, T>(pub HashMap<String, SymbolValue<'a, T>>);
 pub type ConcreteContext<'a> = Context<'a, BigRational>;
 
 impl<T> Context<'_, T> {
-    pub fn get_variable<'n>(&self, name: &'n str) -> Result<&T, EvalError<'n>> {
+    pub fn get_variable(&self, name: &str) -> Result<&T, EvalError> {
         if let Some(value) = self.0.get(name) {
             if let SymbolValue::Num(n) = value {
                 Ok(n)
             } else {
-                Err(EvalError::NotAVariable(name))
+                Err(EvalError::NotAVariable(name.to_owned()))
             }
         } else {
-            Err(EvalError::UnknownVariable(name))
+            Err(EvalError::UnknownVariable(name.to_owned()))
+        }
+    }
+
+    pub fn get_function(&self, name: &str) -> Result<&FuncDef<'_>, EvalError> {
+        if let Some(value) = self.0.get(name) {
+            if let SymbolValue::Func(f) = value {
+                Ok(f)
+            } else {
+                Err(EvalError::NotAFunction(name.to_owned()))
+            }
+        } else {
+            Err(EvalError::UnknownFunction(name.to_owned()))
         }
     }
 
@@ -38,21 +50,45 @@ impl<T> Context<'_, T> {
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum EvalError<'a> {
-    UnknownVariable(&'a str),
-    UnknownFunction(&'a str),
-    NotAVariable(&'a str),
-    NotAFunction(&'a str),
+#[derive(Clone, Debug, PartialEq)]
+pub enum EvalError {
+    UnknownVariable(String),
+    UnknownFunction(String),
+    NotAVariable(String),
+    NotAFunction(String),
+    Arity {
+        function: String,
+        expected: usize,
+        actual: usize,
+    },
     DivisionByZero,
 }
 
-impl<'input> Expr<'input> {
-    pub fn evaluate(&self,
-        ctx: &ConcreteContext<'_>,
-        scoring_callback: &mut impl FnMut(&'static str, Vec<BigRational>),
-    ) -> Result<BigRational, EvalError<'input>> {
+impl<'a> Expr<'a> {
+    pub fn evaluate<'ctx>(&self,
+        ctx: &'ctx ConcreteContext<'a>,
+        scoring_callback: &mut impl FnMut(&str, &[BigRational]),
+    ) -> Result<BigRational, EvalError> {
         Ok(match self {
+            &Expr::Func(id, ref args) => {
+                let func = ctx.get_function(id)?;
+                if func.argument_names.len() != args.len() {
+                    return Err(EvalError::Arity {
+                        function: id.to_owned(),
+                        expected: func.argument_names.len(),
+                        actual: args.len(),
+                    });
+                }
+
+                let values = args.iter()
+                    .map(|arg| arg.evaluate(ctx, scoring_callback))
+                    .collect::<Result<Vec<_>, _>>()?;
+                scoring_callback(id, &values);
+                let fn_ctx = Context(func.argument_names.iter().map(|&name| name.to_owned())
+                                       .zip(values.into_iter().map(SymbolValue::Num))
+                                       .collect());
+                func.value_expr.evaluate(&fn_ctx, scoring_callback)?
+            }
             Expr::Ident(id) => ctx.get_variable(id)?.clone(),
             Expr::BinOp(lhs, op, rhs) => {
                 let lhs = lhs.evaluate(ctx, scoring_callback)?;
@@ -123,16 +159,26 @@ pub enum Pred<'a> {
 mod tests {
     use super::*;
 
-    fn empty_ctx(expr: &str) -> Result<BigRational, EvalError<'_>> {
+    fn empty_ctx(expr: &str) -> Result<BigRational, EvalError> {
         let ctx = ConcreteContext::new();
         with_ctx(&ctx, expr)
     }
 
-    fn with_ctx<'a>(ctx: &ConcreteContext<'_>, expr: &'a str) -> Result<BigRational, EvalError<'a>> {
+    fn with_ctx<'a>(ctx: &ConcreteContext<'_>, expr: &'a str) -> Result<BigRational, EvalError> {
         let expr = ExprParser::new()
             .parse(expr)
             .unwrap();
         expr.evaluate(ctx, &mut |_, _| ())
+    }
+
+    fn test_scoring<'a>(expected: &'a mut Vec<(&'static str, Vec<BigRational>)>)
+        -> impl FnMut(&str, &[BigRational]) + 'a
+    {
+        move |func, args| {
+            let index = expected.iter().position(|&(f, ref a)| f == func && args == &a[..])
+                .expect("unexpected call to scoring callback");
+            expected.swap_remove(index);
+        }
     }
 
     #[test]
@@ -152,7 +198,7 @@ mod tests {
 
     #[test]
     fn unknown_variable() {
-        assert_eq!(empty_ctx("3 * x"), Err(EvalError::UnknownVariable("x")));
+        assert_eq!(empty_ctx("3 * x"), Err(EvalError::UnknownVariable("x".to_owned())));
     }
 
     #[test]
@@ -160,5 +206,41 @@ mod tests {
         let mut ctx = ConcreteContext::new();
         ctx.0.insert("x".to_owned(), SymbolValue::Num(BigRational::from_integer(7.into())));
         assert_eq!(with_ctx(&ctx, "3 * x"), Ok(BigRational::from_integer(21.into())));
+    }
+
+    #[test]
+    fn handles_function() {
+        let mut ctx = ConcreteContext::new();
+        ctx.0.insert("f".to_owned(), SymbolValue::Func(FuncDef {
+            argument_names: vec!["x"],
+            value_expr: ExprParser::new().parse("x*x").unwrap(),
+        }));
+
+        let expr = ExprParser::new().parse("f(3) + 4").unwrap();
+        let mut scoring_calls = vec![
+            ("f", vec![BigRational::from_integer(3.into())]),
+        ];
+
+        let value = expr.evaluate(&ctx, &mut test_scoring(&mut scoring_calls));
+        assert_eq!(value, Ok(BigRational::from_integer(13.into())));
+        assert!(scoring_calls.is_empty());
+    }
+
+    #[test]
+    fn detects_arity_error_early() {
+        let mut ctx = ConcreteContext::new();
+        ctx.0.insert("f".to_owned(), SymbolValue::Func(FuncDef {
+            argument_names: vec!["x"],
+            value_expr: ExprParser::new().parse("x*x").unwrap(),
+        }));
+
+        let expr = ExprParser::new().parse("f(2, f(7))").unwrap();
+        let mut scoring_calls = vec![];
+        let value = expr.evaluate(&ctx, &mut test_scoring(&mut scoring_calls));
+        assert_eq!(value, Err(EvalError::Arity {
+            function: "f".to_owned(),
+            expected: 1,
+            actual: 2,
+        }));
     }
 }
