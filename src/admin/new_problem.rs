0e088945c6507746224e5c00d1344223e5d7d609
Jakub Kądziołka <kuba@kadziolka.net>
2020-05-27 00:11:52 +0200

    Attempt using one embed for many messages
    This results in the following compiler errors:
    
    error[E0597]: `embed` does not live long enough
      --> src/admin/new_problem.rs:16:25
       |
    15 |                     user.dm(&mctx, |m| m.embed(|e| {
       |                                    --- value captured here
    16 |                         embed.title("`<please enter problem name>`")
       |                         ^^^^^---------------------------------------
       |                         |
       |                         borrowed value does not live long enough
       |                         returning this value requires that `embed` is borrowed for `'static`
    ...
    29 |         })),
       |         - `embed` dropped here while still borrowed
    
    error[E0499]: cannot borrow `embed` as mutable more than once at a time
      --> src/admin/new_problem.rs:22:13
       |
    15 |                     user.dm(&mctx, |m| m.embed(|e| {
       |                                    --- first mutable borrow occurs here
    16 |                         embed.title("`<please enter problem name>`")
       |                         --------------------------------------------
       |                         |
       |                         first borrow occurs due to use of `embed` in closure
       |                         returning this value requires that `embed` is borrowed for `'static`
    ...
    22 |             embed.title(&name);
       |             ^^^^^ second mutable borrow occurs here
    
    error[E0499]: cannot borrow `embed` as mutable more than once at a time
      --> src/admin/new_problem.rs:24:28
       |
    15 |                     user.dm(&mctx, |m| m.embed(|e| {
       |                                    --- first mutable borrow occurs here
    16 |                         embed.title("`<please enter problem name>`")
       |                         --------------------------------------------
       |                         |
       |                         first borrow occurs due to use of `embed` in closure
       |                         returning this value requires that `embed` is borrowed for `'static`
    ...
    24 |             user.dm(&mctx, |m| m.embed(|e| {
       |                            ^^^ second mutable borrow occurs here
    25 |                 embed .description("`<please enter problem description>`")
       |                 ----- second borrow occurs due to use of `embed` in closure
    
    error: aborting due to 3 previous errors

--- src/admin/new_problem.rs
+++ src/admin/new_problem.rs
@@ -7,22 +7,23 @@ fn new_problem(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
     let maybe_name = args.remains().map(|s| s.to_owned());
     InteractiveCommand {
         generator: Gen::new_boxed(sync_producer!({
+            let mut embed = serenity::builder::CreateEmbed::default();
+            embed.color(Color::BLUE);
             let name = match maybe_name {
                 Some(name) => name,
                 None => {
-                    user.dm(&mctx, |m| m.embed(|e| e
-                        .color(Color::BLUE)
-                        .title("`<please enter problem name>`")))
-                    .context("Send embed").log_error();
+                    user.dm(&mctx, |m| m.embed(|e| {
+                        embed.title("`<please enter problem name>`")
+                    })).context("Send embed").log_error();
                     yield_!(())
                 }
             };
 
-            user.dm(&mctx, |m| m.embed(|e| e
-                .color(Color::BLUE)
-                .title(&name)
-                .description("`<please enter problem description>`")))
-            .context("Send embed").log_error();
+            embed.title(&name);
+
+            user.dm(&mctx, |m| m.embed(|e| {
+                embed .description("`<please enter problem description>`")
+            })).context("Send embed").log_error();
             let description = yield_!(());
             log::info!("TODO: Create problem {:?} with description {:?}", name, description);
         })),
